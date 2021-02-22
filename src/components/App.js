d55e6726d29e4da6c271698cf933db7fe0022fc0
eubank87 <eubank.ashley@gmail.com>
2021-02-22 14:56:37 -0800

    started home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import { ThemeProvider } from '@material-ui/styles';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 import Header from "./ui/Header";
 import theme from "./ui/Theme";
+import Home from "./Home"
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Header/>
-        <h1>Portfolio The Second</h1>
+        <Header value={value} 
+        setValue={setValue}/>
         <Switch>
-          <Route exact path="/" component={()=> <div>Home</div>}/>
+          <Route exact path="/" render={(props) => <Home {...props} setValue={setValue}/>} />
           <Route exact path="/about" component={()=> <div>About</div>}/>
           <Route exact path="/portfolio" component={()=> <div>Portfolio</div>}/>
           <Route exact path="/contact" component={()=> <div>Contact</div>}/>
---
+++ src/components/Home.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {makeStyles, useTheme} from "@material-ui/core/styles";
+import {Link} from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+
+import flowers from "../assets/flower_background.jpg";
+
+const useStyles = makeStyles(theme=> ({
+    buttonContainer: {
+        marginTop: "1em"
+    },
+    mainContainer: {
+        marginTop: "5em",
+        [theme.breakpoints.down("md")]: {
+            marginTop: "3em"
+        },
+        [theme.breakpoints.down("xs")]: {
+            marginTop: "2em"
+        }
+    },
+    heroTextContainer: {
+        minWidth: "21.5em",
+        marginLeft: "1em",
+        [theme.breakpoints.down("xs")]: {
+            marginLeft: 0
+        }
+    }
+}))
+
+export default function HomePage(props){
+    const classes = useStyles();
+    const theme = useTheme();
+
+    return(
+        <Grid container className={classes.mainContainer} direction="column">
+            <Grid item>
+            <Grid container 
+                justify="flex-end" 
+                alignItems="center" 
+                direction="row">
+                    <Grid sm
+                    className={classes.heroTextContainer} 
+                    item>
+                        <Typography align="center" 
+                        variant="h2">
+                            Ashley Eubank <br/> Full Stack Web Developer
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
